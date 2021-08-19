e129bdd13ef0d958df74c86d011f3932f9e9c4b6
tannerpedretti <tannerpedretti@gmail.com>
2021-08-19 11:21:14 -0500

    demo

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -20,35 +20,47 @@ function LoginForm() {
     );
   };
 
+  const handleDemo = (e) => {
+    e.preventDefault()
+    return dispatch(sessionActions.thunk_loginDemo({credential:"Demo-lition",password:"password"}))
+  }
+
   return (
+    <div>
     
-    <form className="Form" onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <div className="container"></div>
-      <label>
-        Username or Email
-        <input
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+      <form className="Form" onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <div className="container"></div>
+        <label>
+          Username or Email
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <button type="submit">Log In</button>
+      </form>
+      <form onSubmit={handleDemo}>
+            <input type="hidden" name="username" value="Demo-lition"></input>
+            <input type="hidden" name="password" value="password"></input>
+            <button type="submit">Login as Demo User</button>
+      </form>
+    </div>
 
   );
 }
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -8,6 +8,7 @@ import './Navigation.css';
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
 
+
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
